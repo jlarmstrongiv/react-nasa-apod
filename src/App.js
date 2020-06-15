f61f5c8c513bcef5c4d6bcdfb27a22fd797c9fd0
John L. Armstrong IV <jlarmst@g.clemson.edu>
2020-06-15 13:58:27 -0400

    feat: push to github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    apod: null,
+  };
+  // componentWillUnmount () {
+
+  // }
+  // componentDidUpdate() {
+
+  // }
+  componentDidMount() {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then((response) => this.setState({ apod: response.data }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <span>Hello World!</span>
+        {this.state.apod && (
+          <React.Fragment>
+            <h1>{this.state.apod.title}</h1>
+            <p>{this.state.apod.explanation}</p>
+            <iframe
+              title={this.state.apod.title}
+              width="560"
+              height="315"
+              src={this.state.apod.url}
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullscreen
+            ></iframe>
+          </React.Fragment>
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
